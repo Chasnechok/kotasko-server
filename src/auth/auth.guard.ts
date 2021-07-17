afbcfef7a06424e08a66d5283f7a37c649958391
Artem <chasnechok@outlook.com>
2021-07-17 19:04:52 +0200

    auth guard improved

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,5 +1,7 @@
 import { CanActivate, ExecutionContext, Global, Injectable, UnauthorizedException } from '@nestjs/common'
 import { Reflector } from '@nestjs/core'
+import { WsException } from '@nestjs/websockets'
+import { Response } from 'express'
 import { SESSION_PARSER } from 'src/main'
 import { User } from 'src/users/user.schema'
 
@@ -9,21 +11,13 @@ export class AuthGuard implements CanActivate {
     constructor(private reflector: Reflector) {}
 
     async canActivate(context: ExecutionContext): Promise<boolean> {
-        try {
-            const session = await this.getSession(context)
-
-            if (!session || !session.user) {
-                throw new Error()
-            }
-            const user: User = session.user
-            const roles = this.reflector.get<string[]>('roles', context.getHandler())
-            if (!roles) {
-                return true
-            }
-            return this.compareRoles(roles, user.roles)
-        } catch (error) {
-            throw new UnauthorizedException('User is not authorized!')
+        const session = await this.getSession(context)
+        const user: User = session.user
+        const roles = this.reflector.get<string[]>('roles', context.getHandler())
+        if (!roles) {
+            return true
         }
+        return this.compareRoles(roles, user.roles)
     }
 
     private compareRoles(allowedRoles: string[], userRoles: string[]): boolean {
@@ -34,10 +28,16 @@ export class AuthGuard implements CanActivate {
         if (context.getType() == 'ws') {
             const client = context.switchToWs().getClient()
             const handshake = client.handshake
-            await new Promise((res) => SESSION_PARSER(handshake, context.switchToHttp().getResponse(), res))
+            await new Promise((res) => SESSION_PARSER(handshake, {} as Response, res))
+            if (!handshake.session || !handshake.session.user) {
+                throw new WsException('User is not authorized!')
+            }
             return handshake.session
         }
         const req = context.switchToHttp().getRequest()
+        if (!req.session || !req.session.user) {
+            throw new UnauthorizedException('User is not authorized!')
+        }
         return req.session
     }
 }
